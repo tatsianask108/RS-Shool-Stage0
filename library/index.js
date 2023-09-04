8d0a499bf4bdea0affdee43de89efcf4afcb5d1f
tatsianask108 <tt.laurie@gmail.com>
2023-09-04 18:17:31 +0300

    feat: buy books

--- library/index.js
+++ library/index.js
@@ -4,7 +4,7 @@
 function burger() {
   const burger = document?.querySelector('[data-burger]');
   const nav = document?.querySelector('[data-nav]');
-//changed
+  //changed
   const icon = document?.querySelector('[data-auth-icon]');
   const navItems = nav?.querySelectorAll('a');
   const body = document.body;
@@ -16,16 +16,16 @@ function burger() {
   });
 
   document.addEventListener('click', (e) => {
-      if(e.target === nav || nav.contains(e.target) || e.target === burger ) {
-        return false;
-      } else if (body.classList.contains('stop-scroll') || e.target === icon) {
-        // ?????? || e.target === icon
-        body.classList.remove('stop-scroll');
-        burger?.classList.remove('burger--active');
-        nav?.classList.remove('nav--visible');
-        e.preventDefault(true);
-      }
-    });
+    if (e.target === nav || nav.contains(e.target) || e.target === burger) {
+      return false;
+    } else if (body.classList.contains('stop-scroll') || e.target === icon) {
+      // ?????? || e.target === icon
+      body.classList.remove('stop-scroll');
+      burger?.classList.remove('burger--active');
+      nav?.classList.remove('nav--visible');
+      e.preventDefault(true);
+    }
+  });
 
   navItems.forEach(el => {
     el.addEventListener('click', () => {
@@ -53,168 +53,271 @@ function sliderFunction() {
       </div>`,
     activeDotClass = 'active';
 
-      function initSlider() {
+  function initSlider() {
 
-        
 
-        // init dots
-        let dotsHtml = '',
-            slidesPerPage = Math.floor(slider.offsetWidth / sliderItems[0]?.offsetWidth)?? 3;
-            
-        for (let i = 0; i < Math.ceil(slidesCount - slidesPerPage + 1); i++) {
-          dotsHtml += dotHtml;
-        }
 
-        dotsContainer.innerHTML = dotsHtml;
+    // init dots
+    let dotsHtml = '',
+      slidesPerPage = Math.floor(slider.offsetWidth / sliderItems[0]?.offsetWidth) ?? 3;
 
-        // init actions
-        nextButton.addEventListener('click', nextSlide)
-        prevButton.addEventListener('click', prevSlide)
+    for (let i = 0; i < Math.ceil(slidesCount - slidesPerPage + 1); i++) {
+      dotsHtml += dotHtml;
+    }
 
-        for (const [key, item] of Object.entries(dotsContainer.children)) {
-          item.index = key;
-          item.addEventListener('click', function(e) {
-            setActiveSlide(this.index);
-          })
-        }
-        
-        setActiveSlide(0);
+    dotsContainer.innerHTML = dotsHtml;
+
+    // init actions
+    nextButton.addEventListener('click', nextSlide)
+    prevButton.addEventListener('click', prevSlide)
+
+    for (const [key, item] of Object.entries(dotsContainer.children)) {
+      item.index = key;
+      item.addEventListener('click', function (e) {
+        setActiveSlide(this.index);
+      })
+    }
+
+    setActiveSlide(0);
+  }
+
+  function nextSlide() {
+    setActiveSlide(currentSlide === slidesCount - 1 ? currentSlide : currentSlide + 1);
+  }
+  function prevSlide() {
+    setActiveSlide(currentSlide === 0 ? 0 : currentSlide - 1);
+  }
+
+  function setActiveSlide(index = 0) {
+    index = +index;
+
+    position = 475 * index;
+    sliderLine.style.left = -position + 'px';
+
+    if (index > 0) {
+      prevButton.classList.remove('disabled');
+
+      if (index === slidesCount - 1) {
+        nextButton.classList.add('disabled');
+      } else {
+        nextButton.classList.remove('disabled');
       }
+    } else {
+      nextButton.classList.remove('disabled');
+      prevButton.classList.add('disabled');
+    }
 
-      function nextSlide() {
-        setActiveSlide(currentSlide === slidesCount - 1? currentSlide : currentSlide + 1);
+    for (let [key, dot] of Object.entries(dotsContainer.children)) {
+
+      if (key == index) {
+        dot.classList.add(activeDotClass)
+      } else {
+        dot.classList.remove(activeDotClass);
       }
-      function prevSlide() {
-        setActiveSlide(currentSlide === 0? 0 : currentSlide - 1);
-      }
-
-      function setActiveSlide(index = 0) {
-        index = +index;
-
-        position = 475 * index;
-        sliderLine.style.left = -position + 'px';
-
-        if(index > 0) {
-          prevButton.classList.remove('disabled');
-
-          if(index === slidesCount - 1) {
-            nextButton.classList.add('disabled');
-          } else {
-            nextButton.classList.remove('disabled');
-          }
-        } else {
-          nextButton.classList.remove('disabled');
-          prevButton.classList.add('disabled');
-        }
-
-        for (let [key, dot] of Object.entries(dotsContainer.children)) {
-    
-          if(key == index) {
-            dot.classList.add(activeDotClass)
-          } else {
-            dot.classList.remove(activeDotClass);
-          }
-        }
+    }
 
 
-        currentSlide = index;
-      }
+    currentSlide = index;
+  }
 
-      initSlider();
-      window.addEventListener('resize', initSlider);   
+  initSlider();
+  window.addEventListener('resize', initSlider);
 }
 setTimeout(sliderFunction, 500);
 
 
-
 function Account() {
 
-  var userData = {
-      first_name: null, last_name: null, email: null
-    },
-    /**
-     * data-auth-{object_key}: html
-     */
-    userDomDataMap = {
-      '$readers-card-number': 'Math.ceil(Math.random()*1000000000)',
-      '$user-full-name': `userData.first_name + ' ' + userData.last_name`,
-      '$user-short-name': `(userData.first_name.charAt() + userData.last_name.charAt()).toUpperCase()`,
-      //'$user-short-name': 'functionName()',
-      'menu': `
+  let userData = {
+    first_name: null, last_name: null, email: null, full_name: null,
+    boughtBooks: {},
+    is_bought_library_card: false, is_auth: false
+  }
+  /**
+   * data-auth-{object_key}: html
+   */
+  let userDomDataMap = {
+    '$readers-card-number': 'Math.ceil(Math.random()*1000000000)',
+    '$user-full-name': `userData.full_name`,
+    '$user-short-name': `(userData.first_name.charAt() + userData.last_name.charAt()).toUpperCase()`,
+    //'$user-short-name': 'functionName()',
+    'menu': `
         <button data-popup-button="profile-popup">
             <p class="login-dropdown">My profile</p>
         </button>
-        <button onclick="localStorage.setItem('isAuth', false); location.reload()">
+        <button onclick="localStorage.setItem('is_auth', false); location.reload()">
             <p class="login-dropdown">Log Out</p>
         </button>
       `,
-      'find-card-title': 'Your Library card',
-      'get-card': `<div class="get-card-title">Get a reader card</div>
+    'find-card-title': 'Your Library card',
+    'get-card': `<div class="get-card-title">Get a reader card</div>
       <div class="get-card-description">With a digital library card you get free access to the Library’s wide array of digital resources including e-books, databases, educational resources, and more.</div>
       <div class="get-card-buttons">
           <button class="card-button" data-popup-button='profile-popup'>Profile</button>
       </div>
       `,
-      // '$readers-name': `setAttribute('value', userData.first_name + ' ' + userData.last_name)`,
-      // '$readers-card-number': `document.querySelector('[data-auth-readers-card-number]').setAttribute('value', 'card-number')`
-      '$icon': `(userData.first_name.charAt() + userData.last_name.charAt()).toUpperCase()`,
-    };
+    '$bought-books-list': 'getBoughtBooksList()',
+    '$readers-input-name': 'getReadersInputName()',
+    '$bought-books-count': `Object.keys(userData.boughtBooks).length`,
 
-    function functionName() {return "any data"}
+    // '$readers-name.value': `userData.first_name + ' ' + userData.last_name`,
+    // '$readers-card-number': `document.querySelector('[data-auth-readers-card-number]').setAttribute('value', 'card-number')`
 
-    function init() {      
-      if(auth()) {
-        showUserData();
-        initUserActions();
+  };
+
+  function init() {
+    loadUserData();
+
+    if (isAuth()) {
+      showUserData();
+      initActions();
+
+    } else if (isRegistered()) {
+      document.getElementById('Favorites')?.querySelectorAll('.card-button')?.forEach(button => {
+        button.setAttribute('data-popup-button', 'login-popup')
+      });
+    }
+  }
+
+  function isRegistered() {
+    return localStorage.getItem('email') !== null;
+  }
+
+  function isAuth() {
+    return userData.is_auth === true;
+  }
+
+  // load user data from local storage into the userData object
+  function loadUserData() {
+
+    userData.first_name = localStorage.getItem('first_name');
+    userData.last_name = localStorage.getItem('last_name');
+    userData.email = localStorage.getItem('email');
+    userData.full_name = localStorage.getItem('first_name') + ' ' + localStorage.getItem('last_name');
+    userData.is_bought_library_card = JSON.parse(localStorage.getItem('is_bought_library_card'));
+    userData.is_auth = JSON.parse(localStorage.getItem('is_auth'));
+
+    try {
+      let boughtBooksLocalStorageObject = JSON.parse(localStorage.getItem('boughtBooks'));
+
+      if (boughtBooksLocalStorageObject !== null && typeof boughtBooksLocalStorageObject === 'object') {
+        userData.boughtBooks = boughtBooksLocalStorageObject;
       }
+    } catch { }
+  }
+
+  // write user data from userData object into the local storage
+  function updateLocalStorage() {
+    for (let [key, value] of Object.entries(userData)) {
+      if (typeof value === 'object') {
+        value = JSON.stringify(value);
+      }
+      localStorage.setItem(key, value);
+    }
+  }
+
+  function showUserData() {
+
+    // works with DataMap
+    for (let [key, value] of Object.entries(userDomDataMap)) {
+      if (key.includes('$')) {
+        key = key.replace('$', '');
+        value = eval(value);
+      }
+
+      document.querySelectorAll('[data-auth-' + key + ']')?.forEach(item => {
+        item.innerHTML = value;
+      })
+      // document.getElementById('form-libr-card').querySelector('input').value = `${userData.first_name} ${userData.last_name}`;
+      // console.log(document.querySelector('.icon-instead')).title = `${(userData.first_name.charAt() + userData.last_name.charAt()).toUpperCase()}`
     }
 
-    function auth() {
-      if(localStorage.getItem('isAuth') === 'true') {
-        userData = {
-          first_name: localStorage.getItem('userFirstName'),
-          last_name: localStorage.getItem('userLastName'),
-          email: localStorage.getItem('userEmail'),
-        };
-        return true;
-      }
-      return false;
-    }
-    function showUserData() {
-      for (let [key, value] of Object.entries(userDomDataMap)) {
-        if(key.includes('$')) {
-          key = key.replace('$', '');
-          value = eval(value);
-        }
+    // works with books 
+    let allBuyButtons = document.getElementById('Favorites')?.querySelectorAll('.card-button');
+    for (let [key, value] of Object.entries(userData.boughtBooks)) {
+      let button = allBuyButtons.item(key);
 
-        document.querySelectorAll('[data-auth-' + key + ']')?.forEach(item => {
-          item.innerHTML = value;
-        })
-      }
+      button.innerHTML = 'Own';
+      button.classList.add('disabled');
     }
-    function initUserActions() {
-      let allBooks = document.getElementById('Favorites')
-      let allBuyButtons = allBooks.querySelectorAll('.card-button')
-      console.log(localStorage.getItem('boughtLibraryCard'))
+  }
+
+  function getReadersInputName() {
+    return isAuth() ?
+      `<input class="form-input" type="text" placeholder="Reader's name" value="` + userData.full_name + `">` :
+      '';
+  }
+
+  function getBoughtBooksList() {
+    let resultHtmlList = '';
+
+    for (let [key, value] of Object.entries(userData.boughtBooks)) {
+      resultHtmlList += '<li>' + value.author + ', ' + value.name + '</li>'
+    }
+
+    return resultHtmlList;
+  }
+
+  function initActions() {
+    let allBuyButtons = document.getElementById('Favorites')?.querySelectorAll('.card-button')
+
+
+    if (userData.is_bought_library_card === true) {
+
+      allBuyButtons.forEach((button, index) => {
+        let bookContainer = button.parentElement,
+          bookData = {
+            name: bookContainer.querySelector('[data-book-name]')?.innerHTML,
+            author: bookContainer.querySelector('[data-book-author]')?.innerHTML
+          };
+        button.addEventListener('click', addBook.bind(this, bookData, index))
+      })
+
+    } else {
+
       allBuyButtons.forEach(button => {
         button.setAttribute('data-popup-button', 'buy-library-card-popup')
-    });
-    document.getElementById('form-buy-library-card').addEventListener('submit', () => {
-      localStorage.setItem('boughtLibraryCard', true)
+      });
+
+    }
+
+    function addBook(bookData, id) {
+      // 1. add the book into the userData object
+      // 2. update local storage variable according to the userData object
+      // 3. update view stage
+      userData.boughtBooks[id] = bookData;
+      updateLocalStorage();
+      showUserData();
+    }
+
+    // init library cart form
+    document.getElementById('form-buy-library-card')?.addEventListener('submit', () => {
+      userData.is_bought_library_card = true;
+      updateLocalStorage();
     })
 
-    if(localStorage.getItem('boughtLibraryCard') == 'true') {
-      allBuyButtons.forEach(button => {
-        button.removeAttribute('data-popup-button')
-        button.addEventListener('click', () => {
-        button.classList.add('card-button-disabled')
-        button.innerHTML = 'Own'
-        })
-        //??????
-      })}
+
+    // document.getElementById('change-name').addEventListener('submit', (e) => {
+    //   e.preventDefault();
+    //   if (e.target.first_name && e.target.first_name?.value === '') {
+    //     return
+    //   }
+
+    //   userData.first_name = e.target.first_name.value;
+    //   showUserData();
+
+    // })
+
+    /// name changer
+    /// 1. add event listener
+    /// 2. get form data 
+    /// 3. update userData object
+    /// 4. Sync data with storage
+    /// 5. Update user data on the page
+
 
   }
-    init();
+  init();
 }
 
 Account();
@@ -224,7 +327,7 @@ Account();
 // FAVORITES
 /*стили в style.css находятся после строки - "СКРЫТИЕ И ПОЯВЛЕНИЕ КНИГ ПО СЕЗОНАМ" */
 const booksContainer = document.getElementById('books-container'),
-        categoryFilter = document.getElementById('books-category');
+  categoryFilter = document.getElementById('books-category');
 
 categoryFilter.addEventListener('change', () => {
   booksContainer.setAttribute('data-current', categoryFilter.season.value);
@@ -236,11 +339,11 @@ const formRegistration = document.getElementById('form-registration')
 const formRegistrationFields = formRegistration.elements
 
 formRegistration.addEventListener('submit', () => {
-  if(formRegistration.checkValidity()) {
+  if (formRegistration.checkValidity()) {
     for (let i = 0; i < formRegistrationFields.length; i++) {
       localStorage.setItem(formRegistrationFields[i].name, formRegistrationFields[i].value)
     }
-    localStorage.setItem('isAuth', true); 
+    localStorage.setItem('is_auth', true);
   }
 })
 
@@ -248,10 +351,13 @@ formRegistration.addEventListener('submit', () => {
 const formLogin = document.getElementById('form-login')
 const formLoginFields = formLogin.elements
 
-formLogin.addEventListener('submit', () => {
-  if(formLogin.checkValidity()) {
-    if (formLoginFields[0].value == localStorage.getItem('userEmail') && formLoginFields[1].value == localStorage.getItem('userPassword')) {
-      localStorage.setItem('isAuth', true); 
+formLogin.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (formLogin.checkValidity()) {
+    if (formLoginFields[0].value == localStorage.getItem('email') && formLoginFields[1].value == localStorage.getItem('password')) {
+      localStorage.setItem('is_auth', true);
+      location.reload();
     }
   }
 }
@@ -266,43 +372,51 @@ function popUps() {
     popupCloseButtonAttributeName = 'data-popup-close',
     activePopupClass = 'opened';
 
-    document.querySelectorAll('[' + popupButtonAttributeName + ']').forEach((button) => {
-      let popup_id = button.getAttribute(popupButtonAttributeName),
+  document.addEventListener('click', (e) => {
+
+    // if this element or the parent one is Button Close
+    if (e.target.closest('[' + popupCloseButtonAttributeName + ']')) {
+      closeAllPopups();
+      return;
+    }
+
+    // if this element or the parent one is Button Open 
+    let popupButtonOpen = e.target.closest('[' + popupButtonAttributeName + ']');
+
+    if (popupButtonOpen) {
+      let popup_id = popupButtonOpen.getAttribute(popupButtonAttributeName),
         popup = document.getElementById(popup_id);
 
-        if(!popup) {
-          return;
-        }
-
-        button.addEventListener('click', (e) => {
-          openPopup(popup);
-          e.stopImmediatePropagation();
-        });
-        popup.querySelector('[' + popupCloseButtonAttributeName + ']')?.addEventListener('click', closeAllPopups);
-
-    })
-
-    document.addEventListener('click', (e) => {
-      let isPopupParent = e.target?.hasAttribute(popupAttributeName),
-        isPopupChild = e.target.closest('[' + popupAttributeName + ']');
-
-      if(!isPopupParent && isPopupChild) {
-        return false;
+      if (!popup) {
+        return;
       }
 
-      closeAllPopups();
-    })
+      openPopup(popup);
+      e.stopImmediatePropagation();
 
-    function openPopup(popup) {
-      closeAllPopups();     
-      popup.classList.add(activePopupClass);
-    };
+      return;
+    }
 
-    function closeAllPopups() {
-      document.querySelectorAll('[' + popupAttributeName + ']').forEach(item => {
-        item.classList.remove(activePopupClass);
-      }); 
-    };
-    
+    // if e.target is the Parent container of the popup
+    let isPopupParent = e.target?.hasAttribute(popupAttributeName),
+      isPopupChild = e.target.closest('[' + popupAttributeName + ']');
+    if (!isPopupParent && isPopupChild) {
+      return;
+    }
+
+    closeAllPopups();
+  })
+
+  function openPopup(popup) {
+    closeAllPopups();
+    popup.classList.add(activePopupClass);
+  };
+
+  function closeAllPopups() {
+    document.querySelectorAll('[' + popupAttributeName + ']').forEach(item => {
+      item.classList.remove(activePopupClass);
+    });
+  };
+
 }
 popUps();
