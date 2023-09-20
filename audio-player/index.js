9ee9aded2cbd5df16b011582fff798e11fd40d60
tatsianask108 <tt.laurie@gmail.com>
2023-09-20 15:27:37 +0300

    feat: change songs and covers, add numbers to progress bar

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,27 +1,52 @@
-const player = document.querySelector('.player'),
-    playButton = document.querySelector('.play'),
+const audio = document.querySelector('.audio'),
+    player = document.querySelector('.player'),
+    playBtn = document.querySelector('.play'),
     prevButton = document.querySelector('.prev'),
     nextButton = document.querySelector('.next'),
-    audio = document.querySelector('.audio'),
-    songName = document.querySelector('.song-name')
+    artistName = document.querySelector('.artist-name')
+songName = document.querySelector('.song-name')
 progressContainer = document.querySelector('.progress__container'),
     progress = document.querySelector('.progress'),
+    currentTimeSong = document.querySelector('.current-time'),
+    songDuration = document.querySelector('.song-duration'),
     coverImg = document.querySelector('.cover__img'),
     imgSrc = document.querySelector('.img__src'),
     background = document.querySelector('.background')
 
 //song names
-const songs = ['Beyonce', 'Dua Lipa']
+const songs = [
+    {
+        artist: 'LO-FI LE-VI',
+        name: 'You',
+        path: 'assets/audio/LO-FI LE-VI - You.mp3',
+        cover: 'assets/img/cover1.jpg',
+    },
+    {
+        artist: 'Barradeen',
+        name: 'The girl I haven\'t met',
+        path: 'assets/audio/Barradeen - The girl I haven\'t met.mp3',
+        cover: 'assets/img/cover2.jpg',
+    },
+    {
+        artist: 'Extenz',
+        name: 'Life',
+        path: 'assets/audio/Extenz - Life.mp3',
+        cover: 'assets/img/cover3.jpg',
+    }
+
+]
 
 // default song
 let songIndex = 0
 
+
 //initialization
 function loadSong(song) {
-    songName.innerHTML = song
-    audio.src = `assets/audio/${song}.mp3`
-    coverImg.src = `assets/img/cover${songIndex + 1}.png`
-    background.src = `assets/img/cover${songIndex + 1}.png`
+    artistName.innerHTML = song.artist
+    songName.innerHTML = song.name
+    audio.src = `${song.path}`
+    coverImg.src = `assets/img/cover${songIndex + 1}.jpg`
+    background.src = `assets/img/cover${songIndex + 1}.jpg`
 }
 
 loadSong(songs[songIndex])
@@ -40,7 +65,7 @@ function pauseSong() {
     audio.pause()
 }
 
-playButton.addEventListener('click', () => {
+playBtn.addEventListener('click', () => {
     let isPlaying = player.classList.contains('play')
     if (isPlaying) {
         pauseSong()
@@ -48,3 +73,56 @@ playButton.addEventListener('click', () => {
         playSong()
     }
 })
+
+//next song 
+function nextSong() {
+    songIndex++
+    if (songIndex > songs.length - 1) {
+        songIndex = 0
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+nextButton.addEventListener('click', () => {
+    nextSong()
+})
+
+//previous song 
+function prevSong() {
+    songIndex--
+    if (songIndex < 0) {
+        songIndex = songs.length - 1
+    }
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+prevButton.addEventListener('click', () => {
+    prevSong()
+})
+
+//progress bar
+function updateProgress() {
+    currentTimeSong.innerHTML = Math.round(audio.currentTime)
+    songDuration.innerHTML = audio.duration
+    let currentTime = audio.currentTime
+    const duration = audio.duration
+    const progressPercents = (currentTime / duration) * 100
+    progress.style.width = `${progressPercents}%`
+}
+audio.addEventListener('timeupdate', updateProgress)
+
+//change progress bar by click
+function changeProgress(e) {
+    const progressBarWidth = this.clientWidth
+    const clickTarget = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = clickTarget / progressBarWidth * duration
+}
+
+progressContainer.addEventListener('click', changeProgress)
+
+//autoplay
+audio.addEventListener('ended', nextSong)
