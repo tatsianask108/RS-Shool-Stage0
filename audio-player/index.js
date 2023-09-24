92840f22ca2b8b59655c77bd58b02d1145d4341d
tatsianask108 <tt.laurie@gmail.com>
2023-09-24 19:11:59 +0300

    feat: final refactoring

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,12 +1,13 @@
 console.log('1.Вёрстка +10\n2.Кнопка Play/Pause +10\n3.При кликах по кнопкам "Вперёд" и "Назад" переключается проигрываемый аудиотрек. Аудиотреки пролистываются по кругу - после последнего идёт первый +10\n4.При смене аудиотрека меняется изображение - обложка аудиотрека +10\n5.Прогресс-бар отображает прогресс проигрывания текущего аудиотрека. При перемещении ползунка вручную меняется текущее время проигрывания аудиотрека +10\n6.Отображается продолжительность аудиотрека и его текущее время проигрывания +10')
 
 const audio = document.querySelector('.audio'),
-    player = document.querySelector('.player'),
+    player = document.querySelector('#player'),
     playBtn = document.querySelector('.play'),
     prevButton = document.querySelector('.prev'),
-    nextButton = document.querySelector('.next'),
-    artistName = document.querySelector('.artist-name'),
-    songName = document.querySelector('.song-name'),
+    // my own context using player instead of document
+    nextButton = player.querySelector('.next'),
+    artistName = player.querySelector('.artist-name'),
+    songName = player.querySelector('.song-name'),
     // progressContainer = document.querySelector('.progress__container'),
     // progress = document.querySelector('.progress'),
     progressBar = document.querySelector('.progress-bar'),
@@ -24,51 +25,32 @@ const songs = [
         name: 'Silent Wood',
         path: 'assets/audio/Oaasis - Silent Wood.mp3',
         cover: 'assets/img/cover1.jpg',
-        duration: 187.689796,
     },
     {
         artist: 'LO-FI LE-VI',
         name: 'You',
         path: 'assets/audio/LO-FI LE-VI - You.mp3',
         cover: 'assets/img/cover2.jpg',
-        duration: 71.136,
     },
     {
         artist: 'Barradeen',
         name: 'The girl I haven\'t met',
         path: 'assets/audio/Barradeen - The girl I haven\'t met.mp3',
         cover: 'assets/img/cover3.jpg',
-        duration: 91.742041,
     },
     {
         artist: 'Extenz',
         name: 'Life',
         path: 'assets/audio/Extenz - Life.mp3',
         cover: 'assets/img/cover4.jpg',
-        duration: 198.922449,
     },
 
 ]
 
 // default song
-let songIndex = 0
-
-//initialization
-function loadSong(song) {
-    artistName.innerHTML = song.artist
-    songName.innerHTML = song.name
-    audio.src = `${song.path}`
-    // seekBar.max = audio.duration
-    progressBar.max = song.duration
-    coverImg.src = song.cover
-    background.src = song.cover
-    currentTimeSong.innerHTML = '00:00'
-    audio.addEventListener("loadedmetadata", () => {
-        songDuration.innerHTML = formatTime(audio.duration)
-    })
-}
-
-loadSong(songs[songIndex])
+let songIndex = 0,
+    isPlay = false,
+    songProgressInterval
 
 //formatting time to minutes and seconds
 function formatTime(time) {
@@ -83,23 +65,53 @@ function formatTime(time) {
     return `${min}:${sec}`
 }
 
+//initialization
+function loadSong(song) {
+    artistName.innerHTML = song.artist
+    songName.innerHTML = song.name
+    audio.src = song.path
+    coverImg.src = song.cover
+    background.src = song.cover
+    currentTimeSong.innerHTML = '00:00'
+    audio.addEventListener("loadedmetadata", () => {
+        songDuration.innerHTML = formatTime(audio.duration)
+        progressBar.max = audio.duration
+    })
+
+}
+
+loadSong(songs[songIndex])
+
 //play song
 function playAudio() {
-    player.classList.add('play')
+    // player.classList.add('play')
+
+    // visually change progress-bar
+    songProgressInterval = setInterval(() => {
+        progressBar.value = audio.currentTime
+        currentTimeSong.innerHTML = formatTime(audio.currentTime)
+    }, 500)
+
+    isPlay = true
     imgSrc.src = `assets/svg/pause.png`
     audio.play()
 }
 
 //pause song
 function pauseAudio() {
-    player.classList.remove('play')
+    // player.classList.remove('play')
+
+    clearInterval(songProgressInterval)
+
+    isPlay = false
     imgSrc.src = `assets/svg/play.png`
     audio.pause()
 }
 
+
 playBtn.addEventListener('click', () => {
-    let isPlaying = player.classList.contains('play')
-    if (isPlaying) {
+    // let isPlaying = player.classList.contains('play')
+    if (isPlay) {
         pauseAudio()
     } else {
         playAudio()
@@ -134,6 +146,15 @@ prevButton.addEventListener('click', () => {
     playPrev()
 })
 
+//change progress bar by clicking on it
+progressBar.addEventListener('change', () => {
+    audio.currentTime = progressBar.value
+})
+
+//autoplay
+audio.addEventListener('ended', playNext)
+
+
 // //progress bar done with div
 // function updateProgress() {
 //     let currentTime = audio.currentTime
@@ -154,18 +175,3 @@ prevButton.addEventListener('click', () => {
 // }
 
 // progressContainer.addEventListener('click', changeProgress)
-
-
-// visually change progress-bar
-setInterval(() => {
-    progressBar.value = audio.currentTime
-    currentTimeSong.innerHTML = formatTime(audio.currentTime)
-}, 500)
-
-//change progress bar by clicking on it
-progressBar.addEventListener('change', () => {
-    audio.currentTime = progressBar.value
-})
-
-//autoplay
-audio.addEventListener('ended', playNext)
