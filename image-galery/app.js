0c8bada67266df6e06572834ed463e27dd2b8860
tatsianask108 <tt.laurie@gmail.com>
2023-09-28 12:50:46 +0300

    feat: set default image layout

--- image-galery/app.js
+++ image-galery/app.js
@@ -4,8 +4,7 @@ const inputEl = document.getElementById('input')
 const imgContainer = document.getElementById('img-container');
 const form = document.querySelector('.search-bar')
 const cross = document.getElementById('cross')
-let inputData
-// inputData = inputEl.value
+let inputData = ''
 
 
 // ???
@@ -13,23 +12,26 @@ let inputData
 
 async function getData(inputData) {
     inputData = inputEl.value
+    console.log(inputData)
+    if (!inputData) {
+        inputData = 'image'
+    }
     const url = `https://api.unsplash.com/search/photos?query=${inputData}&per_page=9&client_id=${accessKey}`
     const result = await fetch(url);
     const data = await result.json();
-    console.log(data.results);
-    // let photos = data.result
-    data.results.forEach((el) => {
+    console.log(data)
+    if (data.results.length === 0) {
+        const emptyDiv = document.createElement('div')
+        emptyDiv.classList.add('textDiv')
+        emptyDiv.innerHTML = `No images found :(`
+        imgContainer.append(emptyDiv);
+    } else data.results.forEach((el) => {
         const divForImg = document.createElement('div')
         divForImg.classList.add('divUploadedImg')
-        divForImg.innerHTML = `<img src="${el.urls.regular}" alt="" class="gotImg">`
-        // divForImg.style.backgroundImage = el.urls.regular
-        // img.alt = `image`;
-        // console.log(el.urls.regular);
-        console.log(inputEl.value);
+        divForImg.innerHTML = `<img src="${el.urls.regular}" title="${el.alt_description}" class="gotImg">`
         imgContainer.append(divForImg);
+        // divForImg.onclick()
     })
-
-
 }
 getData(inputData);
 // нужно ли вызывать?
